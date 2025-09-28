3040cb755367e7183d0544a9003f84246e66b75e
Graciela <esthertorrecilla98@gmail.com>
2025-09-27 20:18:31 -0500

    Creación del servicio empleado y el controlador para consumo REST

--- src/main/java/gm/rh/servicio/EmpleadoServicio.java
+++ src/main/java/gm/rh/servicio/EmpleadoServicio.java
@@ -1,4 +1,12 @@
 package gm.rh.servicio;
 
+import gm.rh.modelo.Empleado;
+
+import java.util.List;
+
 public interface EmpleadoServicio {
+    List<Empleado> listarEmpleado();
+    Empleado obtenerEmpleadoPorId(Long id);
+    Empleado guardarEmpleado(Empleado empleado); //crea o actualiza
+    void eliminarEmpleado(Long id);
 }
